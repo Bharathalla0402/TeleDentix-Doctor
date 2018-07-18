46fcd0baaeeef3f5ea1cd05cc3ef1f8b7e77c0f2
Rajinder Singh <think360@think360s-mac-mini-2.local>
2018-07-18 15:34:59 +0530

    Initial Commit

---
+++ TeleDentix Doctor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeleDentix Doctor
+//
+//  Created by think360 on 18/07/18.
+//  Copyright © 2018 bharath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
